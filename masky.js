45951e282cca69edd83f55e38b1b466b8a283d70
richbess22 <richbess22@gmail.com>
2025-11-03 22:48:49 +0300

    Update

--- masky.js
+++ masky.js
@@ -1,22 +1,6 @@
 // masky.js
 const images = [
-  "https://files.catbox.moe/3q2jfy.png",
-  "https://files.catbox.moe/3q2jfy.png",
-  "https://files.catbox.moe/h6z5uu.png",
-  "https://files.catbox.moe/sdqimq.png",
-  "https://files.catbox.moe/wtqmpn.png",
-  "https://files.catbox.moe/7k4hun.png",
-  "https://files.catbox.moe/tfjfuq.png",
-  "https://files.catbox.moe/sdqimq.png",
-  "https://files.catbox.moe/l3aiyj.png",
-  "https://files.catbox.moe/vftjyw.png",
-  "https://files.catbox.moe/9kbkft.png",
-  "https://files.catbox.moe/yofo5o.png",
-  "https://files.catbox.moe/l3aiyj.png",
-  "https://files.catbox.moe/vftjyw.png",
-  "https://files.catbox.moe/vftjyw.png",
-  "https://files.catbox.moe/rckmq2.jpg",
-  "https://files.catbox.moe/zfhbst.png"
+  "https://files.catbox.moe/ebj284.jpg"
 ];
 
 function getImage() {
